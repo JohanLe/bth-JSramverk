6c1c7521426a26e74d8acd9eb45d9c86754a846c
Johan Ledel <johan@Johans-MacBook-Pro.local>
2019-09-10 16:10:13 +0200

    report section for Kmom01 done

--- src/js/components/Report.js
+++ src/js/components/Report.js
@@ -2,11 +2,28 @@ import React from 'react';
 import {HeaderReports} from './Header.js';
 
 
-
 class Report extends React.Component {
-
     render() {
-        console.log(this.props.match);
+        const kmom = this.props.match.params.kmom;
+        // const [questions, setQuestions] = useState([]);
+        var questions = [];
+        console.log(this.props.match.params.kmom);
+        switch (this.props.match.params.kmom) {
+            case "1":
+                questions = kmomOne;
+                break;
+        }
+
+        const QuestionsList = () =>
+            questions.map((question, index) => (
+                <div className="question" key={index}>
+                    <p>
+                        <strong>{question.question}</strong>
+                    </p>
+                    <p>{question.answer}</p>
+                </div>
+            ));
+
         return (
 
             <article className="article">
@@ -16,6 +33,7 @@ class Report extends React.Component {
                 </div>
 
                 <div className="article_main_content">
+                    <QuestionsList/>
                 </div>
 
             </article>
@@ -23,14 +41,27 @@ class Report extends React.Component {
     }
 }
 
-var kmomOne= {
-    questions: [
-        "Github"
-    ],
-    answers: [
-        ""
-    ]
+var kmomOne = [
+    {
+        question: "Starta appen", answer: "I en node miljö:" +
+            "Installera Serve: \"npm install -g serve\" " +
+            "Stå i me-app mappen och använd: \"serve -s build\" " +
+            "Öppna webbläsaren på 'localhost:5000' eller vald port"
+    },
+    {question: "Installera moduler", answer: "Först installera Node med npm.\n" +
+            "\n" +
+            "Modulerna installers via npm och npx.\n" +
+            "\n" +
+            "Bootstrapa projektet med\n" +
+            "\n" +
+            "\"npx create-react-app my-app-name\"\n" +
+            "Moduler till detta projekt\n" +
+            "\n" +
+            "Stå i projekt mappen & \"npm install react-router-dom --save\"\n" +
+            "\"npm install -g sass\""},
+    {question: "Github:", answer: "https://github.com/JohanLe/bth-ramverk1"},
+    {question: "", answer: "(Behöver hitta ett sätt att formerta texten bättre)"},
+];
 
-}
 
 export default Report;
