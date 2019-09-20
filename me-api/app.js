275748ea5e4b69407af3402e71dc7b4aa62fa148
Johan Ledel <johan@Johans-MacBook-Pro.local>
2019-09-20 14:12:06 +0200

    moved api and me-app to same repository

---
+++ me-api/app.js
@@ -0,0 +1,55 @@
+const bodyParser = require("body-parser");
+
+const morgan = require('morgan');
+const cors = require('cors');
+const express = require("express");
+
+const app = express();
+const port = 1337;
+
+const index = require('./routes/index.js');
+const hello = require('./routes/hello.js');
+
+app.use(cors());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+// This is middleware called for all routes.
+// Middleware takes three parameters.
+app.use((req, res, next) => {
+    console.log(req.method);
+    console.log(req.path);
+    next();
+});
+
+
+app.use('/', index);
+app.use('/hello', hello);
+
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.status || 500).json({
+        "errors": [
+            {
+                "status": err.status,
+                "title": err.message,
+                "detail": err.message
+            }
+        ]
+    });
+});
+
+
+// Start up server
+app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+
+
